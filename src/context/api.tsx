46eb9132df4a9b280d65b38e4e38b84b770cac5c
Iván Guerrero <ivanguebri@gmail.com>
2024-07-05 21:33:11 -0500

    feat: se obtienen los personajes iniciales

--- src/context/api.tsx
+++ src/context/api.tsx
@@ -1,11 +1,28 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
+
+import { getCharacters } from "../service/rick-and-morty";
 
 import { IApiContext } from "../interfaces/context";
+import { ICharacter } from "../interfaces/api";
 
 export const ApiContext = createContext<IApiContext | undefined>(undefined);
 
 export const ApiContextProvider = ({ children }: PropsWithChildren) => {
-  const [characters, setCharacters] = useState();
+  const [characters, setCharacters] = useState<ICharacter[]>();
+  const [searchCriteria, setSearchCriteria] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const charactersResponse = await getCharacters({
+          filters: { name: searchCriteria },
+        });
+        setCharacters(charactersResponse.results);
+      } catch {
+        console.log();
+      }
+    })();
+  }, [searchCriteria]);
 
   if (characters === undefined) return <>Loading...</>;
   return (
