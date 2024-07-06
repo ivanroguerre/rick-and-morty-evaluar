b9229f99791589a9d97e6f5c3427382e6159ce22
Iván Guerrero <ivanguebri@gmail.com>
2024-07-06 08:35:59 -0500

    feat: searchbar con componente chakra

--- src/component/searchbar.tsx
+++ src/component/searchbar.tsx
@@ -2,12 +2,15 @@ import { useContext } from "react";
 
 import { ApiContext } from "../context/api";
 
+import { Input } from "@chakra-ui/react";
+
 const Searchbar = () => {
   const apiContext = useContext(ApiContext);
   return (
-    <input
+    <Input
       value={apiContext?.searchCriteria}
       onChange={(e) => apiContext?.setSearchCriteria(e.target.value)}
+      placeholder="Nombre del personaje"
     />
   );
 };
